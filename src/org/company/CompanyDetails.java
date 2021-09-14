d8ecf1039769c0075326c9514ca63c102c898aa5
maheuma14 <umaa9310@gmail.com>
2021-09-14 10:13:20 +0530

    message recevied

---
+++ src/org/company/CompanyDetails.java
@@ -0,0 +1,23 @@
+package org.company;
+
+public class CompanyDetails {
+	private void companyName() {
+		System.out.println("Compamny Name is Delta");
+		}
+private void comapnyId() {
+	System.out.println("Company Id is 98383399");
+
+
+}	
+private void companyAddress() {
+	System.out.println("company Address iis Trichy");
+
+}
+public static void main(String[] args) {
+	CompanyDetails cd =new CompanyDetails();
+	cd.companyName();
+	cd.companyAddress();
+	cd.comapnyId();
+}
+
+}
