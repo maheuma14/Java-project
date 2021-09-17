833fd328a087422600a219f40f3513fde8b4548f
maheuma14 <umaa9310@gmail.com>
2021-09-17 10:34:18 +0530

    task completed

---
+++ src/org/company/Class1.java
@@ -0,0 +1,27 @@
+package org.company;
+
+public class Class1 {
+	private void testMethod1() {
+		System.out.println("first");
+		
+	}
+private void testMethod2() {
+	
+		System.out.println("Second");
+	}
+
+private void testMethod3() {
+	System.out.println("Third");
+	
+}
+public static void main(String[] args) {
+	Class1 c = new Class1();
+	c.testMethod1();
+	c.testMethod2();
+	c.testMethod3();
+	
+}
+
+
+
+}
